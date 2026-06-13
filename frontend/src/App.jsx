188530b0543d1bfd04b3059cd1a18c99399801e1
Ubuntu <ubuntu@ip-172-31-25-121.ec2.internal>
2026-06-13 10:16:51 +0000

    frontend upate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,17 @@ import axios from 'axios'
 
 function App() {
   const [message, setMessage] = useState('')
-  const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+  const API_URL = import.meta.env.VITE_API_URL || 'http://54.160.162.210:3000';
   useEffect(() => {
-    axios.get('http://localhost:3000/api/hello')
+    axios.get('{API_URL}:3000/api/hello')
       .then(response => setMessage(response.data.message))
       .catch(error => console.error('Error:', error))
   }, [])
 
   return (
-    <div>
-      <h1>React + Vite + Node.js</h1>
+    <div style={{ maxWidth: '800px', margin: '50px auto', textAlign: 'center' }}>
+      <h1>React + Vite on GitHub Pages</h1>
+      <p>Backend on EC2 + Database on RDS PostgreSQL</p>
       <p>{message}</p>
     </div>
   )
